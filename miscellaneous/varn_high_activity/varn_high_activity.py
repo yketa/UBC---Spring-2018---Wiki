1616ebbcb188ced00842f5bb1357f0996462998d
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2018-11-03 19:28:47 +0100

    msd to analyse C44 at high activity

--- miscellaneous/varn_high_activity/varn_high_activity.py
+++ miscellaneous/varn_high_activity/varn_high_activity.py
@@ -1,11 +1,18 @@
 """
-This script plots local packing fraction distributions at (phi = 0.80, vzero =
-1e-2, dr = 2e-5) and (phi = 0.80, vzero = 1e-1, dr = 5e-4). (see
+This script plots local packing fraction distributions and mean square
+displacements at (phi = 0.80, vzero = 1e-2, dr = 2e-5) and
+(phi = 0.80, vzero = 1e-1, dr = 5e-4). (see
 https://yketa.github.io/UBC_2018_Wiki/#Strain%20correlations%20at%20high%20activity)
 
 File varN_Vj1000_Rg2000.pickle contains a list of densities at (phi = 0.80,
 vzero = 1e-2, dr = 2e-5) and file varN_Vk1000_Rh5000.pickle contains a list of
 densities at (phi = 0.80, vzero = 1e-1, dr = 5e-4).
+
+Files msd_Vj1000_Rg2000_I[initial frame index].csv contain mean square
+displacements at (phi = 0.80, vzero = 1e-2, dr = 2e-5) computed with different
+initial frames. Files msd_Vk1000_Rh5000_I[initial frame index].csv contain mean
+square displacements at (phi = 0.80, vzero = 1e-1, dr = 5e-4) computed with
+different initial frames.
 
 Execution of this script requires the active_particles package (see
 https://github.com/yketa/active_particles).
@@ -19,8 +26,10 @@ import numpy as np
 
 from active_particles.init import set_env
 set_env('SHOW', True)
+from active_particles.exponents import float_to_letters
 
 from active_particles.analysis.varn import histogram, Plot
+from active_particles.plot.plot import list_linestyles
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -33,9 +42,15 @@ if __name__ == '__main__':
     Nbins = 100     # number of bins for the histogram
     phimax = 1.4    # maximum local density for histogram
 
-    # PLOT
+    init = [500, 1000, 2000, 5000]  # initial frame indexes in mean square displacement computations
+
+    dr = {'Vj1000_Rg2000': 2e-5, 'Vk1000_Rh5000': 5e-4} # rotation diffusion rates corresponding to abbreviated directory names
+
+    # PLOT PARAMETERS
 
     mpl.rcParams.update({'font.size': 14})
+
+    # LOCAL PACKING FRACTION PLOT
 
     plot = Plot(suptitle=False)
 
@@ -82,5 +97,41 @@ if __name__ == '__main__':
     plot.fig.suptitle(r'$N = 1\cdot10^5, \phi = 0.80$' + '\n'
         + r'$S_{init}=%d, S_{max}=%d, N_{cases}=%d, l=%d$'
         % (5000, 10, 500, 10))
+
+    # MEAN SQUARE DISPLACEMENT PLOT
+
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel(r'$\tilde{\nu}_r \Delta t$')
+    ax.set_ylabel(r'$\left<|\Delta\vec{r}(\Delta t)|^2\right>/\Delta t$')
+
+    msd = {'Vj1000_Rg2000':{}, 'Vk1000_Rh5000': {}}
+    colors = {'Vj1000_Rg2000': line_Vj1000_Rg2000.get_color(),
+        'Vk1000_Rh5000': line_Vk1000_Rh5000.get_color()}
+    linestyles = list_linestyles(init)
+
+    for dir in msd:
+        for ini in init:
+            msd[dir][ini] = np.genfromtxt(
+                'msd_%s_I%s.csv' % (dir, float_to_letters(ini)),
+                delimiter=',', skip_header=True)
+            ax.loglog(
+                dr[dir]*msd[dir][ini][:, 0],
+                msd[dir][ini][:, 1]/msd[dir][ini][:, 0],
+                color=colors[dir], linestyle=linestyles[ini])
+
+    ax.add_artist(plt.legend(ncol=2, handles=
+        [Line2D([0], [0], color=colors['Vj1000_Rg2000'],
+            label=r'$\tilde{v} = %.2e, \tilde{\nu}_r = %.2e$' % (1e-2, 2e-5)),
+        Line2D([0], [0], color=colors['Vk1000_Rh5000'],
+            label=r'$\tilde{v} = %.2e, \tilde{\nu}_r = %.2e$' % (1e-1, 5e-4))]
+        + list(map(
+            lambda ini: Line2D([0], [0],
+                color='black', linestyle=linestyles[ini],
+                label=r'$S_{init} = %d$' % ini),
+            init))))
+
+    fig.suptitle(r'$N = 1\cdot10^5, \phi = 0.80$' + '\n'
+        + r'$S_{max} = %d, S_{period} = %d$' % (1, 50))
 
     plt.show()
